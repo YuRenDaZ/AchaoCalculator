cb26423f85f61d2ad54426cae7dfcaeb061c4e45
XxxxFR <1184164360@qq.com>
2019-03-23 09:33:06 +0800

    First commit

---
+++ XxxxFR/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Random ran = new Random();
+           
+            string b = "";
+            float  result=0;
+            string a = "";
+            Console.WriteLine("请输入需要生成题目个数：");
+            int n = Convert .ToInt32 (Console.ReadLine ());
+            for (int i=0;i<n;i++)
+            {
+                int   x = ran.Next(0, 8);
+                int   num1 = ran.Next(0, 100);
+                int   num2 = ran.Next(0, 100);
+                int   num3 = ran.Next(0, 100);
+               
+                while (num1 + num2 < num3 && x == 1)//使结果不为负
+                {
+                    x = ran.Next(0, 8);
+                }
+                while ((num2 % num3) != 0 && x == 3)//使num2/num3能得到整数
+                {
+                    x = ran.Next(0, 8);
+                }
+                while (x == 3 && num3 == 0)//使被除数不为零
+                {
+                    x = ran.Next(0, 8);
+                }
+                while (num1<(num2*num3) && x==4)
+                {
+                    x = ran.Next(0, 8);
+                }
+                while ((num2 % num3) != 0 && x == 5)
+                {
+                    x = ran.Next(0, 8);
+                }
+                while (x == 5 && num3 == 0)
+                {
+                    x = ran.Next(0, 8);
+                }
+                while (x == 5 && num1 < num2/num3)
+                {
+                    x = ran.Next(0, 8);
+                }
+                while ((num1*num2) % num3 != 0 && x == 6)
+                {
+                    x = ran.Next(0, 8);
+                }
+                while (x == 6 && num3 == 0)
+                {
+                    x = ran.Next(0, 8);
+                }
+
+
+                switch (x)
+                {
+                    case 0:
+                        a = "+";
+                        b = "+";
+                        result = num1 + num2 + num3;
+                        break;
+                    case 1:
+                        a = "+";
+                        b = "-";
+                        result = num1 + num2 - num3;
+                        break;
+                    case 2:
+                        a = "+";
+                        b = "*";
+                        result = num1 + num2 * num3;
+                        break;
+                    case 3:
+                        a = "+";
+                        b="/";
+                        result = num1 + num2 / num3;
+                        break;
+                    case 4:
+                        a = "-";
+                        b = "*";
+                        result = num1 - num2 * num3;
+                        break;
+                    case 5:
+                        a = "-";
+                        b = "/";
+                        result = num1 - num2 / num3;
+                        break;
+                    case 6:
+                        a = "*";
+                        b = "/";
+                        result = num1 * num2 / num3;
+                        break;
+                }
+                Console.WriteLine(num1.ToString () + a +num2 +b + num3 +"="+ result);
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ XxxxFR/ConsoleApp2/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1c42c1c3-ad65-4542-8b0c-04112b3452a7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
