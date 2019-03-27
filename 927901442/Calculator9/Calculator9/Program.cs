898243b954808d9200268e31bf94d897bdba11f9
unknown <927901442@qq.com>
2019-03-27 21:52:11 +0800

    First commit

---
+++ 927901442/Calculator9/Calculator9/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("需要多少道算术题?");
+            int k = int.Parse(Console.ReadLine());
+            for(int i=0;i<k;i++)
+            {
+                byte[] buffer = Guid.NewGuid().ToByteArray();
+                int iseed1 = BitConverter.ToInt32(buffer, 0);
+               
+                Random num1 = new Random(iseed1);
+                
+                float a = num1.Next(1, 40);
+                Random num2 = new Random();
+                float b = num2.Next(1, 40);
+                Random num3 = new Random(iseed1);
+                int c = num3.Next(1, 5);
+                float result = 0;
+                switch(c)
+                {
+                    case 1:
+                        
+                        result = a + b;
+                        Console.WriteLine(a + "+" + b + "=" + result);
+                        break;
+                    case 2:
+                        
+                        result = a - b;
+                        Console.WriteLine(a + "-" + b + "=" + result);
+                        break;
+                    case 3:
+                        
+                        result = a * b;
+                        Console.WriteLine(a + "*" + b + "=" + result);
+                        break;
+                    case 4:
+                        
+                        result = a / b;
+                        Console.WriteLine(a + "/" + b + "=" + result);
+                        break;
+                    default:
+                        break;
+                }
+                
+            }
+        }
+    }
+}
---
+++ 927901442/Calculator9/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("11583069-3083-403a-8c15-b577628113f5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
